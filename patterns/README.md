95665731c1e11e8bb4eded38097d3369dd6f8ad6
anshyyy <anshuman9998@gmail.com>
2026-03-08 15:40:12 +0530

    Add Strategy Design Pattern implementation with payment options

---
+++ patterns/README.md
@@ -0,0 +1,9 @@
+
+
+// design patters are those reusable solutions which are used for common system design problems. 
+They are usually of 3 type 
+
+1️⃣ Creational Patterns (5) : this deals with object creation: making object creation fleixble and reusable
+2️⃣ Structural Patterns (7) : this deals with how clasess and objects are combined to form larger solutions.
+3️⃣ Behavioral Patterns (11) : this focus on how objects communicates with each other
+
---
+++ patterns/creational/StrategyDesignPattern.java
@@ -0,0 +1,102 @@
+// Strategy patten is creational pattern that lets us define a family of algorithm, put each other at different classes ans make their object inter changeable
+//this is used when we have multiple ways to do the same task, and we want to decide a way at runtime without changing the client code
+
+
+/* 
+
+  class PaymentService {
+
+    public void pay(String paymentType, double amount) {
+
+        if(paymentType.equals("CREDIT_CARD")) {
+            System.out.println("Paid " + amount + " using Credit Card");
+        }
+
+        else if(paymentType.equals("UPI")) {
+            System.out.println("Paid " + amount + " using UPI");
+        }
+
+        else if(paymentType.equals("PAYPAL")) {
+            System.out.println("Paid " + amount + " using PayPal");
+        }
+    }
+}
+
+// this is code when we have multple payments options, this can fail as we have to change the client code everytime when we have to do something, 
+this also brokes the open-closed principle
+
+to solve this type of problems we have strategy pattern
+*/
+
+package patterns.creational;
+
+interface PaymentStrategy {
+     public void pay(double amount);
+}
+
+
+class CreditCardPayment implements PaymentStrategy {
+
+      
+    private String creditCardNo;
+
+    CreditCardPayment(String creditCardNo){
+        this.creditCardNo = creditCardNo;
+    }
+
+    @Override
+    public void pay(double amount){
+        System.out.println("Paying with Credit Card No: "+ this.creditCardNo + "amount : "+amount);
+    }
+}
+
+
+
+class UPIPayment implements PaymentStrategy{
+
+    private String upiId;
+
+    UPIPayment(String upiId){
+        this.upiId = upiId;
+    }
+    
+    @Override
+    public void pay(double amount){
+        System.out.println("Paying with UPI id: "+ this.upiId + "amount : "+amount);
+    }
+}
+
+
+class PaymentContext {
+     private PaymentStrategy paymentStrategy;
+     
+     PaymentContext(PaymentStrategy strategy){
+        this.paymentStrategy = strategy;
+     }
+
+     public void processPayment(double amount){
+         this.paymentStrategy.pay(amount);
+     }
+}
+
+
+// PaymentContext context = new PaymentContext(new CreditCardPayment("1234"));
+
+// context.processPayment(1000);
+
+// context = new PaymentContext(new UpiPayment("user@upi"));
+
+// context.processPayment(500);
+
+
+
+
+
+
+
+
+
+
+
+
+
